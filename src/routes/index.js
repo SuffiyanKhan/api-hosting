25478d99aca23a8d739f4e372c7e387f0717d15b
Suffiyan Khan <suffiyanahmed804092@gmail.com>
2024-07-10 03:13:32 +0500

    working

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,13 @@ import rToken from '../controllers/refresh.controller.js';
 const route = express.Router();
 
 
+route.post('/check', (req,res)=>{
+    try {
+        return res.status(200).json({status:200,message:"successfully! welcome to hosting api's"})
+    } catch (error) {
+        return res.status(500).json({message:"faild "})
+    }
+})
 route.post("/signup", signupRouteValidator, signup)
 route.post('/login', login)
 route.post('/logout', logout)
